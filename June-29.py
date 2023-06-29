976c7868b69ee09a991249a7ae93af3906a3a42a
DevikaP03 <137064499+DevikaP03@users.noreply.github.com>
2023-06-29 17:27:38 +0530

    Add files via upload

---
+++ June-29.py
@@ -0,0 +1,21 @@
+# Author: Devika; Purpose: Python coding; Date: June 29,2023
+
+# print statements
+
+print("This is summer school 2023")
+
+x=100
+print("the value of x is: ",x)
+
+# Variables
+
+y = 1000  //integer
+z = "one thousand"  //string
+A = 12.88888888  //float
+
+# Indentations
+print("This is summer school 2023")
+    print("the value of x is: ",x)
+
+if 5>2:
+    print("True")
