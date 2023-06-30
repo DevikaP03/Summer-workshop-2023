32e7965066b6c89a2102e4a7a0c8cac6414da442
DevikaP03 <137064499+DevikaP03@users.noreply.github.com>
2023-06-30 17:29:22 +0530

    Add files via upload

---
+++ Python-basics.py
@@ -0,0 +1,57 @@
+# Author: Devika; Purpose: Python coding; Date: June 29,2023
+
+# print statements
+
+print("This is summer school 2023")
+
+x=100
+print("the value of x is: ",x)
+
+# Variables
+
+y = 1000  //integer
+z = "one thousand"  //string
+A = 12.88888888  //float
+
+# Indentations
+print("This is summer school 2023")
+    print("the value of x is: ",x)
+
+if 5>2:
+    print("True")
+
+# If else statements
+a = 200
+b = 33
+
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is less than a")
+
+    
+# Operators
++
+-
+*
+/
+
+print(10+10)
+print(10*10)
+print(10-10)
+print(10/10)
+
+# Data frames
+
+Matrix, list, vectors....
+
+list {1,2,3,4,5} : single array one-dimensional
+
+matrix: multi dimensional
+
+list ={1,2,3,4,5}
+list_mix = [1,"one",2,"two",3,9.8]
+
+# for loop (in sequence one by one)
+for x in list_mix:
+    print(x)
